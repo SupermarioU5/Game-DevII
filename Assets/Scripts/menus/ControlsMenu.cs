408abdc75d84af16857bba2ebb34d390de0171a7
SupermarioU5 <27abrahjacw@washk12.org>
2025-02-25 12:41:19 -0700

    controls menu actually pauses the game

--- Assets/Scripts/menus/ControlsMenu.cs
+++ Assets/Scripts/menus/ControlsMenu.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class ControlsMenu : MonoBehaviour
 {
-    public void Start()
-    {
-        Time.timeScale = 0f;
-    }
     public GameObject controls;
     private void Update()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        Time.timeScale = 0f;
     }
     public void closeControls()
     {
