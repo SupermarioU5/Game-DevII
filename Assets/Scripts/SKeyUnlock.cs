6e23ac9846e7870c62fce05a0afdbe2744dfcf62
SupermarioU5 <27abrahjacw@washk12.org>
2025-03-05 14:31:32 -0700

    added new key and other
     E button doesn't work

---
+++ Assets/Scripts/SKeyUnlock.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SKeyUnlock : MonoBehaviour
+{
+    // door stuff
+    public GameObject door;
+    public Renderer doorR;
+    private float speed = 4f;
+    private float openHeight;
+    public Transform startPos;
+    private bool touched;
+    public Material mat1;
+    // key stuff
+    public SKeyCollecter keys;
+    private bool keyGet;
+    private void Start()
+    {
+        openHeight = startPos.position.y + startPos.localScale.y - 0.5f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (keys.SkeleKeyGet)
+        {
+            keyGet = true;
+        }
+        if (touched)
+        {
+            door.transform.position = Vector3.Lerp(door.transform.position, new Vector3(door.transform.position.x, openHeight, door.transform.position.z), Time.deltaTime * speed);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (keyGet&&other.CompareTag("Player"))
+        {
+            touched = true;
+            doorR.material=mat1;
+        }
+    }
+}
