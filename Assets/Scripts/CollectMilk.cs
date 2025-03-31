a9af903b1fcf33242981af21e29c3ae9cd86983b
SupermarioU5 <27abrahjacw@washk12.org>
2025-03-31 14:35:15 -0600

    ending added

---
+++ Assets/Scripts/CollectMilk.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollectMilk : MonoBehaviour
+{
+    public GameObject milk;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("TheEnd");
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Destroy(milk);
+        }
+    }
+}
